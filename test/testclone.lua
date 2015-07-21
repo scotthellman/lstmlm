167f039e15ee577324b7588f823a89f62bb288d8
Nicholas Dronen <nicholas.dronen@pearson.com>
2015-07-21 12:51:20 -0600

    Removed cloneThroughTimeDisk.

--- test/testclone.lua
+++ test/testclone.lua
@@ -5,11 +5,9 @@ local model = nn.Sequential()
 model:add(nn.Linear(10, 10))
 model:add(nn.Tanh())
 
-local diskClones = cloneThroughTimeDisk(model, 4)
 local clones = cloneThroughTime(model, 4)
 
 local newVal = 10
 model:parameters()[1][1][1] = newVal
 
-assert(diskClones[1]:parameters()[1][1][1] == newVal)
 assert(clones[1]:parameters()[1][1][1] == newVal)
