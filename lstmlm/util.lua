5d547bfa8e52280887838c593a9e83df96aeb53b
Scott Hellman <scott@scutellaris.com>
2015-07-22 12:15:08 -0600

    cloneThroughTime comments and some cleanup

--- lstmlm/util.lua
+++ lstmlm/util.lua
@@ -1,4 +1,7 @@
 function cloneThroughTime(net, n)
+    --[[for backprop through n timesteps, we need to replicate our network n
+        times, sharing the weights across networks
+    --]]
     local clones = {}
     local params, gradParams
     if net.parameters then
@@ -12,13 +15,13 @@ function cloneThroughTime(net, n)
     for t = 1, n do
         clones[t] = net:clone()
         if net.parameters then
+            --share the parameters across the clones
             local cloneParams, cloneGradParams = clones[t]:parameters()
             for i = 1, #params do
                 cloneParams[i]:set(params[i])
                 cloneGradParams[i]:set(gradParams[i])
             end
         end
-        collectgarbage()
     end
 
     return clones
